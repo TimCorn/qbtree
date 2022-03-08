10ced9e0ecea7ce0cd8c6a0a9a727e3d4027160f
mike <mike@debianKortes.homedomain>
2022-03-08 17:33:11 +0300

    Init

---
+++ test.h
@@ -0,0 +1,26 @@
+﻿// test.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+
+#include <iterator>    /** for ostream_iterator */
+#include <stack>    /** for stack */
+#include <queue>    /** for queue */
+#include <cstring>  /** for strcpy() */
+
+
+
+
+
+
